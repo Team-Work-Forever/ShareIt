af76f2e9791660a9e6e3f2c1ffc940c5a2fc1291
CavasCallahan <dassuncao1234@gmail.com>
2023-01-09 21:34:29 +0000

    invites totally complete!

--- src/main/java/shareit/services/InviteService.java
+++ src/main/java/shareit/services/InviteService.java
@@ -1,12 +1,17 @@
 package shareit.services;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import shareit.data.JobOffer;
 import shareit.helper.Invitation;
+import shareit.helper.ReverseInvite;
 import shareit.repository.GlobalRepository;
 
 @Service
@@ -25,6 +30,35 @@ public class InviteService {
             .stream()
                 .filter(invite -> invite.getId() == id)
                 .findAny();
+
+    }
+
+    public Collection<Invitation> getInboxReverseInvite() {
+
+        Collection<Invitation> invites = globalRepository.getInvites();
+        Collection<Invitation> selectedInvites = new ArrayList<>();
+
+        Iterator<Invitation> it = invites.iterator();
+
+        while( it.hasNext() ) {
+
+            Invitation invite = it.next();
+
+            if (invite.getInvitationType() instanceof ReverseInvite) {
+                
+                if (((ReverseInvite)invite.getInvitationType()).getApplication() instanceof JobOffer) {
+                    if (invite.getExpire().isAfter(LocalDate.now())) {
+                        selectedInvites.add(invite);
+                    } else {
+                        it.remove(); // If this invitation get expired then is removed!
+                    }
+                }
+
+            }
+
+        }
+
+        return selectedInvites;
 
     }
 
